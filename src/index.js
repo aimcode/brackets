ef9be8ea96ef6888c4a127970b6e82e18ebb3eec
aimcode <aimcode2020@gmail.com>
2020-10-20 13:12:51 +0500

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map(el => el.join(""))
+
+    while (true) {
+        let length = str.length
+        for(let i = 0; i < brackets.length; i++) {
+            str = str.replace(brackets[i], "")
+        }
+
+        if (length === str.length) return false
+        if (str.length === 0) return true
+    }
 }
